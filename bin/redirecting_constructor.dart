235f3f0fc9f559a510212fcb843b01097057cbbd
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-27 10:12:41 +0700

    redirecting constructor

--- bin/redirecting_constructor.dart
+++ bin/redirecting_constructor.dart
@@ -8,6 +8,10 @@ class Person {
     : this(name, "no address"); // redirecting constructor
   Person.withAddress(String address)
     : this("no name", address); // redirecting constructor
+  Person.fromJakarta()
+    : this.withAddress("Jakarta"); // redirecting named constructor
+  Person.withNoName()
+    : this.withName("No Name"); // redirecting named constructor
 }
 
 void main() {
@@ -23,4 +27,12 @@ void main() {
   var person2 = Person("Eko Mareko", "Cikarang");
   print(person2.name);
   print(person2.address);
+
+  var person3 = Person.fromJakarta();
+  print(person3.name);
+  print(person3.address);
+
+  var person4 = Person.withNoName();
+  print(person4.name);
+  print(person4.address);
 }
