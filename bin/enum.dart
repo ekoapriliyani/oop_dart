fb6b5faabc67c07bdf560445d84464a363ca5fee
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-29 13:30:53 +0700

    enum

---
+++ bin/enum.dart
@@ -0,0 +1,8 @@
+import 'data/customer.dart';
+
+void main() {
+  var customer = Customer('Eko', CustomerLevel.premium);
+  print(customer.name);
+  print(customer.level);
+  print(CustomerLevel.values);
+}
