5a9502cf881812054d0e4ac6b3b64135c4c72480
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-29 13:10:22 +0700

    typedef function

---
+++ bin/typedef_function.dart
@@ -0,0 +1,10 @@
+// typedef function tidak direkomendasikan karena membingungkan
+typedef Filter = String Function(String);
+
+void sayHello(String name, Filter filter) {
+  print('Hello ${filter(name)}');
+}
+
+void main() {
+  sayHello('Eko', (value) => value.toUpperCase());
+}
