d09e7093f7132c5f610c128c9731ef3456c2c5c6
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-29 13:19:16 +0700

    static field

---
+++ bin/static_field.dart
@@ -0,0 +1,9 @@
+class Aplication {
+  static final String name = 'Belajar Dart';
+  static final String author = 'Eko Apriliyani';
+}
+
+void main() {
+  print(Aplication.name); // bisa diakses tanpa membuat object
+  print(Aplication.author);
+}
