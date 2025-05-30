d1b096e1a323af5dccf49f81d3057947020003eb
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-05-30 15:38:26 +0700

    metadata

--- bin/metadata.dart
+++ bin/metadata.dart
@@ -7,3 +7,19 @@ class Sample {
   @Deprecated("Do not use this anymore")
   void doNotUseThis() {}
 }
+
+class Todo {
+  final String todo;
+  const Todo(this.todo);
+}
+
+@Todo("will be implemented next release")
+class Application {
+  @Todo("will be implemented next release")
+  String? name;
+  @Todo("will be implemented next release")
+  void featureA() {}
+}
+
+// manfaat metadata:
+// nanti jika sudah belajar tentang reflection
